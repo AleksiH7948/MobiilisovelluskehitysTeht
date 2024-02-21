1468a745ea9c17d8d17dcfcecef7ee476d728c8d
Haklu98 <ab7948@student.jamk.fi>
2024-02-21 11:01:54 +0200

    tehty injektio TodoServicelle App.componentissa

--- Teht3/ang-pwakoe/src/app/app.component.ts
+++ Teht3/ang-pwakoe/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ang-pwakoe';
+
+  ts = inject(TodoService);
+
+  ngOnInit(): void {}
 }
--- Teht3/ang-pwakoe/src/app/todo.service.ts
+++ Teht3/ang-pwakoe/src/app/todo.service.ts
@@ -6,4 +6,10 @@ import { Injectable } from '@angular/core';
 export class TodoService {
   apiurl: string = 'https://jsonplaceholder.typicode.com/todos/1';
   constructor() {}
+
+  // getTodo-metodi palauttaa promisen, joka on
+  // Response-tyyppinen.
+  getTodos(): Promise<Response> {
+    return fetch(this.apiurl);
+  }
 }
