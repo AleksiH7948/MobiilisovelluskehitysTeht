4f655b23c34b950a890777b93b787382be8a5b13
Haklu98 <ab7948@student.jamk.fi>
2024-06-04 19:31:34 +0300

    teht4

--- Teht4/ang-pwakartta/src/app/app.module.ts
+++ Teht4/ang-pwakartta/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MapComponent],
+  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Teht4/ang-pwakartta/src/app/map/map.component.ts
@@ -0,0 +1,86 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
+L.Marker.prototype.options.icon = iconDefault;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css',
+})
+export class MapComponent implements AfterViewInit {
+  private map: any;
+  constructor() {}
+  private initMap(): void {
+    // watchPosition-metodi tarkkailee käyttäjän sijaintia
+    // ja päivittää markkeria koko ajan
+    navigator.geolocation.watchPosition((location) => {
+      const latlng = new L.LatLng(
+        location.coords.latitude,
+        location.coords.longitude
+      );
+
+      const lat = location.coords.latitude;
+
+      this.map = L.map('map').setView(latlng, 13);
+
+      // add the OpenStreetMap tiles
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+      }).addTo(this.map);
+
+      // show the scale bar on the lower left corner
+      L.control.scale().addTo(this.map);
+
+      // show a marker on the map
+      L.marker(latlng).bindPopup('The center of the world').addTo(this.map);
+
+      if (lat > 62) {
+        this.notifyMe();
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  notifyMe() {
+    if (!('Notification' in window)) {
+      // Check if the browser supports notifications
+      alert('This browser does not support desktop notification');
+    } else if (Notification.permission === 'granted') {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification('Olet ylittänyt leveypiirin 62');
+      // …
+    } else if (Notification.permission !== 'denied') {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          const notification = new Notification('Hi there!');
+          // …
+        }
+      });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
+}
